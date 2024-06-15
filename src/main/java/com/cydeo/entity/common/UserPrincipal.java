a4893cad04711db8e11ab4a2b2c4b3e4838bf41c
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-06-14 19:35:16 -0700

    Security is done. Created BaseEntityListener, and whenever something change it will be reflected in the DB by the security

--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -58,4 +58,9 @@ public class UserPrincipal implements UserDetails {
     public boolean isEnabled() {
         return this.user.isEnabled();
     }
+
+    public Long getId(){
+        return this.user.getId();
+    }
+
 }
